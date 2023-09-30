b2c057eb812d88f4520c30b47f91613479ac19f2
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-09-30 13:07:55 +0300

    Update 6.py

--- HW2/6.py
+++ HW2/6.py
@@ -1,8 +1,9 @@
 x = input()
-y = int(input())
+y = y0 = int(input())
+
 for i in x:
     if i.isdigit():
         y -= 1
         if not y:
-            print(y, "-ая цифра в строке:", i)
+            print(y0, "-ая цифра в строке:", i)
             break
