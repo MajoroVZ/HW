ea438b0012333677084d23bd8e4e1d048ee6493b
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-09-30 12:52:57 +0300

    Update 4.py

--- HW2/4.py
+++ HW2/4.py
@@ -1,14 +1,7 @@
 x = input()
-k = 0 
-b = 0
-for i in range(len(x)):
-    if x[i] == '(':
-        k += 1
-    if x[i] == ')':
-        b += 1
-if k == b:
-    print("Все скобки есть")
-elif k < b:
-    print("Не хватает открывающейся скобки")
+if x.count(')') == x.count('('):
+    print("Все отлично")
+elif x.count(')') > x.count('('):
+    print("Не хватает (")
 else:
-    print("Не хватает закрывающийся скобки")
+    print("Не хватает )")
