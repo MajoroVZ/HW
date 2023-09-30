c31006189af78b7e0e5ffadc77d27eb85728ede9
MajoroVZ <daniil.major123@gmail.com>
2023-09-30 12:02:11 +0300

    6.py

--- HW2/6.py
+++ HW2/6.py
@@ -1,5 +1,11 @@
-x = [x for x in input().split()]
+x = input()
 y = int(input())
-a = [''.join(map(str,x))]
-f = str(a)
-print(f[y+1])
+a = 0
+b = 0
+for i in x:
+    if i.isdigit():
+        a += 1
+        if a == y:
+            b = i
+            break
+print(y, "-ая цифра в строке:", b)
