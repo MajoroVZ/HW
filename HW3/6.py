7e83c413151b8140f88f72eb78eb5d6d4154af87
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-10-07 11:07:12 +0300

    Update 6.py

--- HW3/6.py
+++ HW3/6.py
@@ -1,14 +1,9 @@
 n = int(input())
 def f(n):
-    k = 0
     for i in range(2,int(n**0.5)+1):
         if n % i == 0:
-            k += 1
-        if k == 1:
             break
-    if k == 0:
-        return True
-    else:
-        return False
+            return False
+    retutn True
 print(f(n))
  
