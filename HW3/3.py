12aacbb76e41b451eec97eec1a2b257a75899224
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-10-07 11:00:48 +0300

    Update 3.py

--- HW3/3.py
+++ HW3/3.py
@@ -3,9 +3,9 @@ def f(n):
     k = 1
     x = 0
     c = 0
-    for i in range(n):
+    for i in range(1, n + 1):
         x = c + k
         print(x, end=" ")
         c = k
         k = x
-print(f(n))
+f(n)
