a38660154e8fa33092a8515247671bdde5fbc848
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-10-14 10:24:42 +0300

    Update 2.py

--- HW3/2.py
+++ HW3/2.py
@@ -1,14 +1,12 @@
 def vremi_goda(n):
     if (n > 0 and n < 3) or n == 12:
         return "Зима"
-    elif n > 2 and n < 6:
+    if n > 2 and n < 6:
         return "Весна"
-    elif n > 5 and n < 9:
+    if n > 5 and n < 9:
         return "Лето"
-    elif n > 8 and n < 12:
+    if n > 8 and n < 12:
         return "Осень"
-    else:
-        return "нет такого месяца"
 
 
 n = int(input())
