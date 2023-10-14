92e392407591f3761bd8eedd59102711220acfb9
MajoroVZ <143869753+MajoroVZ@users.noreply.github.com>
2023-10-14 14:19:00 +0300

    Update one.py

--- HW3/one.py
+++ HW3/one.py
@@ -4,10 +4,8 @@ def f(x):
         a.append(x)
         x = input()
     return a
-<<<<<<< HEAD:HW3/one.py
-=======
 
 
 x = input()
 print(f(x))
->>>>>>> 57865164e05a0ea86a08fb355faf78e68ed38826:HW3/1.py
+
