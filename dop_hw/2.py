0d55e2aceee2e493b16db1e89d63a04d385b263c
MajoroVZ <daniil.major123@gmail.com>
2023-10-20 23:09:27 +0300

    2.dop without bred

--- dop_hw/2.py
+++ dop_hw/2.py
@@ -1,14 +1,12 @@
 def next_max_int(n):
     b = n
-    k1, k2 = '', ''
     i = len(n) - 1
     while int(n[i]) <= int(n[i-1]):
         i -= 1
-    else:
-        k1, k2 = n[i], n[i-1]
-        #n[i], n[i-1] = k2, k1
-        n = n.replace(n[i], k2, 1)
-        n = n.replace(n[i-1], k1, 1)
+    k1, k2 = n[i], n[i-1]
+    #n[i], n[i-1] = k2, k1
+    n = n.replace(n[i], k2, 1)
+    n = n.replace(n[i-1], k1, 1)
     if n == b:
         return -1
     return n
