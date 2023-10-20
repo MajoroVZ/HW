18b19f713f12e4e89957709a2570f0b825107da4
MajoroVZ <daniil.major123@gmail.com>
2023-10-20 22:26:34 +0300

    2.dop

--- dop_hw/2.py
+++ dop_hw/2.py
@@ -1,15 +1,22 @@
 def next_max_int(n):
     b = n
+    max = 0
+    min = 9
+    maxindex = 0
+    minindex = 0
+    for i in range(len(n)):
+        if int(n[i]) >= max:
+            max = int(n[i])
+            maxindex = i
+        if int(n[i]) <= min:
+            min = int(n[i])
+            minindex = i
+
     for i in range(len(n)-1, -1, -1):
-        k = n[i]
-        if int(n[i]) > int(n[i-1]):
-            n = n.replace(n[i], n[i-1],1)
-            n = n.replace(n[i-1], k,1)
-            break
-    if n == b:
-        return -1
-    return n
+        k = [i]
+
+        
 
 n = str(input())
 print(next_max_int(n))
-#1241
+#1243
