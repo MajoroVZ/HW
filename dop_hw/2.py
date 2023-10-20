66152f72cca90ddafc5ca83217ab5975d8015a95
MajoroVZ <daniil.major123@gmail.com>
2023-10-20 23:30:31 +0300

    2 last

--- dop_hw/2.py
+++ dop_hw/2.py
@@ -1,13 +1,12 @@
 def next_max_int(n):
-    b = n
     i = len(n) - 1
-    while int(n[i]) <= int(n[i-1]):
+    while n[i] <= n[i-1]:
         i -= 1
+        if i == 0:
+            return -1
     k1, k2 = n[i], n[i-1]
-    n = n[:i] + k2 + n[i+1:]
-    n = n[:i-1] + k1 + n[i:]
-    if n == b:
-        return -1
+    n = n.replace(n[i], k2, 1)
+    n = n.replace(n[i-1], k1, 1)
     return n
 
 
