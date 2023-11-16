a00765b8b20d8aa8dd3b9f101f372477cffbe739
MajoroVZ <daniil.major123@gmail.com>
2023-11-16 16:42:39 +0300

    HASH123

--- HASH/.pytest_cache/HASH123.py
+++ HASH/.pytest_cache/HASH123.py
@@ -45,12 +45,40 @@ class HashTable:
         return self.size / self.capacity
 
 
-h = HashTable()
-print(h.hash("daniil"))
+def main():
+    Hash = HashTable()
+    while True:
+        print('1)Добавить ключ-значение')
+        print("2)Удалить значение")
+        print("3)Получить значение")
+        print("4)Коэффициент заполнения")
+        print("5)Вывести таблицу")
+        print("6)Выход")
+        inp = input("Ваш выбор: ")
+        if inp == str(1):
+            while True:
+                Hash.set_value(input("Ключ: "), input("Значение: "))
+                if input == '':
+                    break
+        elif inp == str(2):
+            while input:
+                Hash.del_value(input("Введите ключ: "))
+        elif inp == str(3):
+            pass
+        elif inp == str(4):
+            pass
+        elif inp == str(5):
+            print(Hash.buckets)
+        elif inp == str(6):
+            break
+
+
+main()
+'''print(h.hash("daniil"))
 print(h.set_value("daniil", "pads;fj"))
 print(h.set_value("denis", "ta;ldkf;lasdkfa;"))
 print(h.set_value("dadfasdfnis", "ta;ldkf;lasdkfa;"))
 print(h.del_value("daniil"))
 print(h.get_value("daniil" ))
 print(h.load_factor())
-print(h.buckets)
+print(h.buckets)'''
