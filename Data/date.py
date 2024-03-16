1ede7fecf253421f21c808cd91773aa42aa10255
MajoroVZ <daniil.major123@gmail.com>
2024-03-16 14:11:13 +0300

    second_HW_date

--- Data/date.py
+++ Data/date.py
@@ -4,29 +4,70 @@ from datetime import date
 class Date:
     DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    def __init__(self, year=1, month=1, day=1):
+    def __init__(self, day=1, month=1, year=1):
         self.year = year
         self.month = month
         self.day = day
 
     def input_date(self):
         dates = input("Введите дату в формате День.Месяц.Год: ")
-        self.day, self.month, self.year = self.validate_input(dates)
-        self.validate_date()
+        try:
+            self.day, self.month, self.year = self.validate_input(dates)
+            self.validate_date()
+        except TypeError:
+            self.validate_input(dates)
 
     def validate_date(self):
-        if (self.month <= 0 or self.month > 12) or self.day > self.DAYS_IN_MONTH[self.month - 1]:
-            raise ValueError('Неверный формат даты!')
+        if (self.month <= 0 or self.month > 12) or self.day > self.days_in_month(self.month,
+                                                                                 self.year) or self.year < 1:
+            raise DateValidationError('Неверный формат даты!', 'ValueError')
 
     @staticmethod
     def validate_input(dates):
         data = dates.split('.')
         if len(data) == 3 and (data[0].isdigit() and data[1].isdigit() and data[2].isdigit()):
             return map(int, data)
-        raise ValueError('Только цифры!')
+        raise TypeError('Только цифры и неверный формат записи')
+
+    @staticmethod
+    def is_leap_year(y: int) -> bool:
+        """checking leap year"""
+        return (y % 4 == 0 and y % 100 != 0) or not y % 400
+
+    @classmethod
+    def days_in_month(cls, month, year) -> int:
+        """checking the correct number of months"""
+        if month == 2 and cls.is_leap_year(year):
+            return 29
+        return cls.DAYS_IN_MONTH[month - 13]
 
     def __str__(self):
         return f'{self.day}.{self.month}.{self.year}'
+
+    def __add__(self, other):
+        day_sum, month_sum, year_sum = self.day + other.day, self.month + other.month, self.year + other.year
+        while day_sum > self.days_in_month(month_sum, year_sum):
+            day_sum = day_sum - self.days_in_month(month_sum, year_sum)
+            month_sum += 1
+
+        if month_sum > 12:
+            month_sum = month_sum - 12
+            year_sum += 1
+        return self.__class__(day_sum, month_sum, year_sum)
+
+    def __sub__(self, other):
+        year = self.year - other.year
+        month = self.month - other.month
+        if month < 1:
+            month += 12
+            year -= 1
+        day = self.day - other.day
+        if day < 1:
+            month -= 1
+            dop_day = 1 if not year % 4 and month == 2 else 0
+            day = self.days_in_month(month, year) + dop_day + day
+
+        return self.__class__(day, month, year)
 
 
 class DateStamp(Date):
@@ -38,7 +79,14 @@ class DateStamp(Date):
         raise NotImplementedError
 
 
-# d1 = DateStamp()
-# print(d1)
-d1 = Date()
-d1.input_date()
+class DateValidationError(Exception):
+    def __init__(self, message, code):
+        super().__init__(message)
+        self.code = code
+
+
+if __name__ == '__main__':
+    d1 = Date(31, 1, 1012)
+    d2 = Date(31, 1, 1012)
+    print(d1 + d2)
+    print(d1 - d2)
--- Data/test_date.py
+++ Data/test_date.py
@@ -3,6 +3,7 @@ import builtins
 from unittest import mock
 from HW.Data.date import Date
 from HW.Data.date import DateStamp
+from HW.Data.date import DateValidationError
 import freezegun
 
 
@@ -15,7 +16,7 @@ import freezegun
     ]
 )
 def test_init(in_year, in_month, in_day, year, month, day):
-    d1 = Date(in_year, in_month, in_day)
+    d1 = Date(in_day, in_month, in_year)
     assert d1.day == day
     assert d1.month == month
     assert d1.year == year
@@ -41,16 +42,49 @@ def test_input_date(input_values, expected_day, expected_month, expected_year):
     assert d.year == expected_year
 
 
-@pytest.mark.parametrize("input_values, result",
-                         [
-                             ('28.28.2025', 'Неверный формат даты!'),
-                             ("29.02.2024", 'Неверный формат даты!'),
-                         ])
-def test_format_check_exception(input_values, result):
+# @pytest.mark.parametrize("input_values, result",
+#                          [
+#                              ('28.28.2025', 'Неверный формат даты!'),
+#                              ("29.02.2023", 'Неверный формат даты!'),
+#                          ])
+# def test_format_check_exception(input_values, result):
+#     d = Date()
+#     with mock.patch.object(builtins, 'input', lambda _: input_values):
+#         with pytest.raises(ValueError, match=result) as ex:
+#             d.input_date()
+@pytest.mark.parametrize(
+    "day, month, year, error_message",
+    [
+        (32, 1, 1, 'даты'),
+        (15, 13, 1, 'даты'),
+        (1, 1, 0, 'даты'),
+    ]
+)
+def test_validate_date(day, month, year, error_message):
+    date = Date()
+    date.day = day
+    date.month = month
+    date.year = year
+    with pytest.raises(DateValidationError, match=error_message):
+        date.validate_date()
+
+
+@pytest.mark.parametrize(
+    "dates, Error",
+    [
+        ('ы.a.s', 'Только цифры и неверный формат записи'),
+        ('asdf,123,132', 'Только цифры и неверный формат записи'),
+        ('123-123-123', 'Только цифры и неверный формат записи'),
+        ('ы.123.123', 'Только цифры и неверный формат записи'),
+        ('ы.123.444', 'Только цифры и неверный формат записи'),
+        ('ы.a.s', 'Только цифры и неверный формат записи')
+
+    ]
+)
+def test_validate_input(dates, Error):
     d = Date()
-    with mock.patch.object(builtins, 'input', lambda _: input_values):
-        with pytest.raises(ValueError, match=result) as ex:
-            d.input_date()
+    with pytest.raises(TypeError, match=Error):
+        assert d.validate_input(dates)
 
 
 @pytest.mark.parametrize(
@@ -64,7 +98,7 @@ def test_format_check_exception(input_values, result):
     ]
 )
 def test_str_(input_values, expected_day, expected_month, expected_year):
-    date = Date(expected_year, expected_month, expected_day)
+    date = Date(expected_day, expected_month, expected_year)
     assert str(date) == input_values
 
 
@@ -75,8 +109,9 @@ def test_str_(input_values, expected_day, expected_month, expected_year):
     [
         ('0001-1-1', 1, 1, 1),
         ("0001-1-1", 1, 1, 1),
-        ("2023-1-2", 2, 1, 2023),
-        ("2022-2-28", 28, 2, 2022),
+        ("2023-1-2", 2023, 1, 2),
+        ("2022-2-28", 2022, 2, 28),
+        ("2024-2-29", 2024, 2, 29)
 
     ]
 )
@@ -87,3 +122,38 @@ def test_init(input_values, expected_day, expected_month, expected_year):
     assert d1.day == expected_day
     assert d1.month == expected_month
     assert d1.year == expected_year
+
+
+@pytest.mark.parametrize(
+    'd1,m1,y1,d2,m2,y2, d3,m3,y3',
+    [
+        (10, 10, 10, 10, 10, 10, 20, 8, 21),
+        (31, 1, 2024, 31, 3, 2024, 1, 6, 4048),
+        (1, 12, 12, 1, 12, 12, 2, 12, 25),
+    ]
+)
+def test_add(d1, m1, y1, d2, m2, y2, d3, m3, y3):
+    d1 = Date(d1, m1, y1)
+    d2 = Date(d2, m2, y2)
+    f = d1 + d2
+    assert f.day == d3
+    assert f.month == m3
+    assert f.year == y3
+
+
+@pytest.mark.parametrize(
+    'd1,m1,y1,d2,m2,y2, d3,m3,y3',
+    [
+        (15, 3, 2005, 10, 2, 2000, 5, 1, 5),
+        (31, 1, 2000, 1, 1, 1000, 30, 12, 999),
+        (1, 4, 2000, 1, 1, 1001, 28, 2, 999),
+        (1, 4, 2000, 1, 1, 1000, 29, 2, 1000),
+    ]
+)
+def test_sub(d1, m1, y1, d2, m2, y2, d3, m3, y3):
+    d1 = Date(d1, m1, y1)
+    d2 = Date(d2, m2, y2)
+    f = d1 - d2
+    assert f.day == d3
+    assert f.month == m3
+    assert f.year == y3
